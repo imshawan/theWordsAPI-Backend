93370dbc56acbff30e4b650a0f2ff47190d400d4
Shawan Mandal <shawan.sm049@gmail.com>
2021-08-02 14:05:26 +0530

    Added func fetchAudioFromAPI()

--- testAPI.py
+++ testAPI.py
@@ -10,4 +10,15 @@ def fetchFromAPI(query):
     #print(response.headers)
     print(response.text)
 
+def fetchAudioFromAPI(query):
+    '''Fetches pronunciation data from API'''
+    URL = f"https://the-words.herokuapp.com/api/v2/audio/en-US/entries/{query}"
+    HEADERS={'Accept': 'application/json'}
+
+    response = requests.get(URL, headers=HEADERS)
+    print(response.status_code)
+    #print(response.headers)
+    print(response.text)
+
 fetchFromAPI("hello")
+fetchAudioFromAPI("hello")
